ed2b39970cf016b9e35b664cdeed07a0abb358f8
Shivansh Dave <dave.shivansh@gmail.com>
2021-07-29 11:31:09 -0400

    Update script_fig_txn_compare_conc.m

--- script_fig_txn_compare_conc.m
+++ script_fig_txn_compare_conc.m
@@ -8,12 +8,12 @@ DATA = simulate_ORN(PULSE,SpikeEN);
 
 %%
 plt.Lwd = 1.2;
-plt.FTsz = 14;
+plt.FTsz = 16;
 plt.Xoff = 0.1;
 plt.FGpos = [10 10 900 500];
-plt.scale = [1 4];
+plt.scale = [3 12 1];
 plt.ytick = [-50,-25,0];
-plt.xtick = [0:4];
+plt.xtick = -1:4;
 plt.fname = '.\Report\figs\fig_txn_compare.png';
 
 plot_pulse_currents_overlap(plt,DATA)
@@ -21,33 +21,42 @@ plot_pulse_currents_overlap(plt,DATA)
 function plot_pulse_currents_overlap(plt,D)
 
     figure('Renderer', 'painters', 'Position', plt.FGpos);
-    t = tiledlayout(sum(plt.scale),1,'TileSpacing','none','Padding','compact');
+    plt.t = tiledlayout(sum(plt.scale),1,'TileSpacing','none','Padding','compact');
+    plt.X = [D.PULSE.tspan(1)-plt.Xoff, D.PULSE.tspan(2)];
     
     nexttile([plt.scale(1) 1])
     TT = linspace(D.T(1),D.T(end),100);
     OD = simulate_pulse_train(TT,D.PULSE.ton,D.PULSE.toff,D.PULSE.conc);
     OD = OD(end,:);
-    ax1 = plot(TT,OD,'k-','LineWidth',plt.Lwd);
-    set(gca,'XColor','none','YTick', [0 1], 'YTickLabel', [],...
-        'FontSize',plt.FTsz,...
-        'color','none','box', 'off', 'tickdir', 'out')
-    xlim([D.PULSE.tspan(1)-plt.Xoff, D.PULSE.tspan(2)])
+    plt.ax1 = plot(TT,OD,'k-','LineWidth',plt.Lwd);
     ylabel({'Conc.','(uM)'})
+    set(gca,'XLim',plt.X,'XColor','none','XTick', [], 'XTickLabel', [],...
+        'YTick', [0 1], 'YTickLabel', [],...
+        'tickdir', 'out','FontSize',plt.FTsz,...
+        'color','none','box', 'off')
 
     nexttile([plt.scale(2) 1])
     hold on
     for k = 1:size(D.PULSE.ton,1)               
-        axl(k) = plot(D.T,real(D.PRED.Im(:,k)),'-','LineWidth',plt.Lwd);
-%         set(gca,'ColorOrderIndex',k)
+        plt.axl(k) = plot(D.T,real(D.PRED.Im(:,k)),'-','LineWidth',plt.Lwd);
     end
-%     axis on
-    ylim([-50 0])
-    xlim([D.PULSE.tspan(1)-plt.Xoff, D.PULSE.tspan(2)])
-    set(gca,'YTick', plt.ytick,'XTick',plt.xtick,'FontSize',plt.FTsz,...
-        'color','none','box', 'off', 'tickdir', 'out')
     xlabel('Time (sec)')
     ylabel('Cell Current (pA)')
-    legend(num2str(D.PULSE.conc))   
+    lgd = legend(num2str(D.PULSE.conc));
+    title(lgd,'Conc. (uM)')
+    
+    set(gca,'XLim',plt.X,'XColor','none','XTick',[],'XTickLabel',[],...
+        'YLim',[plt.ytick(1) plt.ytick(end)],'YTick',plt.ytick,...
+        'tickdir', 'out','FontSize',plt.FTsz,...
+        'color','none','box', 'off')
+
+    nexttile([plt.scale(3) 1])
+    axis();
+    xlabel('Time (sec)')
+    set(gca,'XLim',plt.X,'XTick',plt.xtick,...
+        'YColor','none','YTick', [], 'YTickLabel', [],...
+        'tickdir', 'out','FontSize',plt.FTsz,...
+        'color','none','box', 'off')
     
     exportgraphics(gcf,plt.fname,'Resolution',300)
 end
