e9b9710ef299ba32c3d07f68558c0236a0f0c779
Shivansh Dave <dave.shivansh@gmail.com>
2021-08-10 22:55:09 -0400

    sniffing-exp

--- script_fig_sniff_freq_FRss.m
+++ script_fig_sniff_freq_FRss.m
@@ -1,34 +1,115 @@
 % script_fig_sniff_freq_FRss
-%% X
-b_frq = 30; % Breaths/min
-conc = [10,50,300]';
-np = 5; 
 
-T=60/b_frq;
-SpikeEN = 1; 
-plt.N = length(conc); 
-PULSE.ton = T*(0:np-1).*ones(plt.N,1);
-PULSE.toff = round(T/3,1) + T*(0:np-1).*ones(plt.N,1);
-PULSE.conc = conc.*ones(1,np).*ones(plt.N,1);
-PULSE.tspan = [-1 np*T+1];
+%% SETUP
+D.test_frq = [5,10,15,20,25,30,35,40,45,50];
+D.conc = [10,20,50,300]';
+D.np = 5; 
+plt.raw_plots = 1;
+plt.FR_plots = 1;
+
+%% COLLECT DATA
+for i=1:length(D.test_frq)
+    b_frq = D.test_frq(i); % Breaths/min    
+    T=60/b_frq;
+    SpikeEN = 1; 
+    plt.N = length(D.conc); 
+    PULSE.ton = T*(0:D.np-1).*ones(plt.N,1);
+    PULSE.toff = round(T/3,1) + T*(0:D.np-1).*ones(plt.N,1);
+    PULSE.conc = D.conc.*ones(1,D.np).*ones(plt.N,1);
+    PULSE.tspan = [-1 D.np*T+1];
+
+    % %
+    disp(['F=',num2str(b_frq)]);
+    DATA = simulate_ORN(PULSE);
+    DATA.spk = script_spikes_ID(real(DATA.PRED.spkV),DATA.T,0);
+    D.(['f',num2str(b_frq)]) = DATA;
+    % %
+    if plt.raw_plots
+        % % Plot
+        plt.Lwd = 1;
+        plt.FTsz = 14;
+        plt.FGpos = [10 10 1000 600];
+        plt.Xoff = 0.2;
+        plt.xtick = -1:4;
+        plt.scale = [1,2,4];
+        plt.fname = ['.\Report\figs\sniff\fig_spk_sniffing_' num2str(b_frq) 'bpm'];
+        plot_one_bfreq(plt,DATA);
+    end
+end
+
+%% FIND FR
+D.FRss = nan(length(D.conc),length(D.test_frq));
+D.FRmx = D.FRss;
+
+for f=1:length(D.test_frq)
+    Dspk = D.(['f',num2str(D.test_frq(f))]);    
+    % LAST
+    dur_stim = [Dspk.PULSE.ton(end),Dspk.PULSE.toff(end)];
+    spk_count = cellfun(@(v) sum(v>=dur_stim(1) & v<=dur_stim(2)), Dspk.spk(:,3)); 
+    D.FRss(:,f) = spk_count/diff(dur_stim);
+    %FIRST
+    dur_stim = [Dspk.PULSE.ton(1),Dspk.PULSE.toff(1)];
+    spk_count = cellfun(@(v) sum(v>=dur_stim(1) & v<=dur_stim(2)), Dspk.spk(:,3)); 
+    D.FRmx(:,f) = spk_count/diff(dur_stim);
+end
 
 %%
-DATA = simulate_ORN(PULSE);
+if plt.FR_plots
+    % % Plot
+    plt.Lwd = 1;
+    plt.FTsz = 16;
+    plt.FGpos = [10 10 900 600];
+    plt.Xoff = 1;
+    plt.xtick = -1:4;
+    plt.scale = [7,1];
+    plt.fname = ['.\Report\figs\fig_sniff_freq_FR_tuning'];
 
-%% Plot
-plt.Lwd = 1;
-plt.FTsz = 14;
-plt.FGpos = [10 10 1000 600];
-plt.Xoff = 0.2;
-plt.xtick = -1:4;
-plt.scale = [1,2,4];
-plt.fname = ['.\Report\figs\sniff\fig_spk_sniffing_' num2str(b_frq) 'bpm.png'];
-
-plot_one_bfreq(plt,DATA);
-
-
+    plot_freq_FRss(plt,D);
+end
 %%
+disp('---DONE---')
 
+
+function plot_freq_FRss(plt,D)
+    %%
+    figure('Renderer', 'painters', 'Position', plt.FGpos);
+    plt.t = tiledlayout(2*plt.scale(1),1,'TileSpacing','tight','Padding','tight');
+    plt.X = [D.test_frq(1)-plt.Xoff, D.test_frq(end)];
+    plt.ax = [];
+
+    % FR Max - First stim
+    plt.ax(end+1) = nexttile([plt.scale(1) plt.scale(2)]);
+    plot(D.test_frq, D.FRmx,'LineWidth',plt.Lwd)
+    hold on
+    set(gca,'ColorOrderIndex',1)
+    scatter(D.test_frq, D.FRmx,75,'^')
+    % xlabel('Time (sec)')
+    ylabel('Peak Spike-rate')
+    set(gca,'XLim',plt.X,'XColor','none','XTick', [], 'XTickLabel', [],...
+            'YTickLabelRotation',0,...
+            'tickdir', 'out','FontSize',plt.FTsz,...
+            'color','none','box', 'off')
+
+    % FR ss - Last stim
+    plt.ax(end+1) = nexttile([plt.scale(1) plt.scale(2)]);
+    plot(D.test_frq, D.FRss,'LineWidth',plt.Lwd)
+    hold on
+    set(gca,'ColorOrderIndex',1)
+    scatter(D.test_frq, D.FRss,75,'^')
+    xlabel('Breathing frequency (bpm)')
+    ylabel('Steady-state Spike-rate')
+    lgd = legend({num2str(D.conc)},'Location','best');
+    title(lgd,'Conc. (uM)') 
+    set(gca,'XLim',plt.X,...
+            'YTickLabelRotation',0,...
+            'tickdir', 'out','FontSize',plt.FTsz,...
+            'color','none','box', 'off')
+        
+
+    linkaxes(plt.ax(:),'x')    
+    exportgraphics(gcf,[plt.fname '.png'],'Resolution',300)
+
+end
 
 function plot_one_bfreq(plt,DATA)
     figure('Renderer', 'painters', 'Position', plt.FGpos);
@@ -39,7 +120,7 @@ function plot_one_bfreq(plt,DATA)
     % PULSE
     plt.ax(end+1) = nexttile([plt.scale(1) plt.scale(2)]);
     TT = linspace(DATA.T(1),DATA.T(end),100);
-    OD = simulate_pulse_train(TT,PULSE.ton,PULSE.toff,PULSE.conc);
+    OD = simulate_pulse_train(TT,DATA.PULSE.ton,DATA.PULSE.toff,DATA.PULSE.conc);
     % OD = OD(end,:);
     plot(TT,OD,'LineWidth',plt.Lwd);
     % legend(num2str(PULSE.conc))
@@ -54,8 +135,7 @@ function plot_one_bfreq(plt,DATA)
     plt.ax(end+1) = nexttile([plt.scale(1) plt.scale(3)]);
     plot(DATA.T,real(DATA.PRED.Im),'LineWidth',plt.Lwd)
     hold on
-    spikes = script_spikes_ID(real(DATA.PRED.spkV),DATA.T,0);
-    dind=8;
+    spikes = DATA.spk; dind=8;
     set(gca,'ColorOrderIndex',1)
     for i=1:plt.N
         scatter(DATA.T(spikes{i,1}-dind),...
@@ -64,7 +144,7 @@ function plot_one_bfreq(plt,DATA)
     % xlabel('Time (sec)')
     ylabel('I_{ORN} (pA)')
     set(gca,'XLim',plt.X,'XColor','none','XTick', [], 'XTickLabel', [],...
-            'YLim',[-55 20],'YTick', [-55 0 20], 'YTickLabelRotation',0,...
+            'YTickLabelRotation',0,...
             'tickdir', 'out','FontSize',plt.FTsz,...
             'color','none','box', 'off')
 
@@ -110,6 +190,9 @@ function plot_one_bfreq(plt,DATA)
             'color','none','box', 'off')
 
     linkaxes(plt.ax(:),'x')    
-    exportgraphics(gcf,plt.fname,'Resolution',300)
+    exportgraphics(gcf,[plt.fname '.png'],'Resolution',300)
+    
+    xlim([DATA.PULSE.ton(end),DATA.PULSE.toff(end)])
+    exportgraphics(gcf,[plt.fname '_last.png'],'Resolution',300)
 
 end
